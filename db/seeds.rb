9eacf4d466ca0b7a0555928806e1981a9074c583
safak mengu <mengusafak@gmail.com>
2022-11-01 21:37:20 +0000

    index-view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+ # 4.times do Flat.new []
+
+
+
+  10.times do |i|
+    puts "Importing movies from page #{i + 1}"
+      Flat.create(
+        name: "flat #{i}",
+        description: 'bla bla bla',
+        price: 100,
+        user_id: 1
+      )
+  end
