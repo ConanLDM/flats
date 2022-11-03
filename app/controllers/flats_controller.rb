d275e2351b5696809f19f8073a4aaa5722b2e75d
Angus Spawton-Rice <adspawtonrice@gmail.com>
2022-11-03 16:31:43 +0000

    'flats' controller updated to allow new entries

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,49 +1,48 @@
 class FlatsController < ApplicationController
-
-  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+  before_action :set_flat, only: %i[show edit update destroy]
+  # before_action :authenticate_user - not yet
 
   def index
     @flats = Flat.all
   end
 
-  def show
-
-  end
+  def show; end
 
   def new
     @flat = Flat.new
   end
 
-
   def create
     @flat = Flat.new(flat_params)
-      if @flat.save
-        redirect_to @flat
-      else
-        render "new"
-      end
+    @flat.user_id = 1
+    if @flat.save
+      redirect_to @flat
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    # TO DO
   end
 
   def update
     @flat = Flat
+    # TO DO
   end
 
   def destroy
-
+    # TO DO
   end
 
   private
 
   def set_flat
     @flat = Flat.find(params[:id])
-
   end
 
   def flat_params
-
+    params.require(:flat).permit(:name, :address, :description, :price_per_day, :image_url, :photo)
   end
 
 end
