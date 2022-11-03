03393f8a6c50e7b7901c4e43345a7765c49d34ba
Angus Spawton-Rice <adspawtonrice@gmail.com>
2022-11-03 12:44:00 +0000

    changed the model, seed and made a start on the index.

--- db/migrate/20221101193309_create_flats.rb
+++ db/migrate/20221101193309_create_flats.rb
@@ -3,7 +3,9 @@ class CreateFlats < ActiveRecord::Migration[7.0]
     create_table :flats do |t|
       t.string :name
       t.text :description
-      t.float :price
+      t.string :address
+      t.string :image_url
+      t.float :price_per_day
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,51 @@
-10.times do |i|
-  puts "Importing movies from page #{i + 1}"
-  Flat.create(
-    name: "flat #{i}",
-    description: 'bla bla bla',
-    price: 100,
-    user_id: 1
-  )
-end
+puts "Destroying records"
+# User.destroy_all
+Flat.destroy_all
+
+User.create(
+  email: "email@email.com",
+  password: "password"
+)
+puts "Dummy user created"
+
+Flat.create(
+  name: "Desk hanging over a volcano",
+  description: "Quite warm, good for cooking - hard to focus with lava in your face but overall decent.",
+  address: "1 Volvano Hole, Vulcaña",
+  image_url: "volcano.jpg",
+  price_per_day: 45,
+  user_id: 1
+)
+puts "Volcano desk created"
+
+Flat.create(
+  name: "Home office in a TREE HOUSE",
+  description: "Live out your childhood dream whilst doing what make adult life suck - working!",
+  address: "123 Grassy Gardens, Lintendon",
+  image_url: "treehouse.jpg",
+  price_per_day: 30,
+  user_id: 1
+)
+puts "Treehouse desk created"
+
+Flat.create(
+  name: "Work from a pub today",
+  description: "Pretend it's about getting a change of scenery and let us ply you with booze all day. It's a win-win!",
+  address: "The Dog & Bell, 116 Prince St, London SE8 3JD",
+  image_url: "pub.gif",
+  price_per_day: 10,
+  user_id: 1
+)
+puts "Pub desk created"
+
+Flat.create(
+  name: "Sit down at Morley's",
+  description: "Can't to heat your own place anymore? Come and see how long our chicken sits there before the evening rush.",
+  address: "Morley's Chicke, 68A Westow Hill, London SE19 1RX",
+  image_url: "morleys.jpg",
+  price_per_day: 5,
+  user_id: 1
+)
+puts "Morley's desk created"
+
+puts "#{Flat.all.size} created"
