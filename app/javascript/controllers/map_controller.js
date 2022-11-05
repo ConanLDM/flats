ffb76e52d4406fa0b4df7acafadf87ccf4b56c58
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2022-11-05 12:14:06 +0000

    new-Mapbox-line

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -25,7 +25,7 @@ export default class extends Controller {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window)
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popu //check thuis!!!!
+        .setPopup(popup) //check thuis!!!!
         .addTo(this.map)
     });
     this.#fitMapToMarkers()
